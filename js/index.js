d23ec43b185f79e697db0ab78006180c05e55594
saraalwayslikeworld <1378761528@qq.com>
2018-07-09 16:43:33 +0800

    完成热门歌榜和搜索页

--- js/index.js
+++ js/index.js
@@ -6,40 +6,47 @@ let indexPage = {
     },
     getLatestMuisc(){     
         $.get('../static/songs.json').then((list)=>{
-            this.render(list)
+            this.render(list,$('.latestMusic>ol'))
         })
         $('.latestMusic>.loading').remove()
     },
-    render(list){
+    render(list,$node){
+        list = list.sort((a,b)=>{return a.rank>b.rank})
         list.forEach(item=>{
             let $li = `
                 <li>
-                    <h3>${item.title}</h3>
-                    <p>
-                        ${item.singer}-${item.album}
-                    </p>
-                    <svg class="icon">
-                        <use xlink:href="#icon-play"></use>
-                    </svg>
-                    <a href="./song.html?id=${item.id}"></a>
-                </li>`
-            if(item.isSQ){
-                $li = `            
-                    <li>
+                <div class="rank">${item.rank>=10?item.rank:'0'+item.rank}</div>
+                <a href="./song.html?id=${item.id}">
+                    <div class="info">
                         <h3>${item.title}</h3>
                         <p>
-                            <svg class="icon">
-                                <use xlink:href="#icon-sq"></use>
-                            </svg>
+                            <svg class="icon"><use xlink:href="#icon-sq"></use> </svg>
                             ${item.singer}-${item.album}
                         </p>
                         <svg class="icon">
                             <use xlink:href="#icon-play"></use>
                         </svg>
-                        <a href="./song.html?id=${item.id}"></a>
-                    </li>`
+                    </div>
+                </a>
+            </li>`
+            if(item.isSQ){
+                $li = `            
+                <li>
+                    <div class="rank">${item.rank>=10?item.rank:'0'+item.rank}</div>
+                    <a href="./song.html?id=${item.id}">
+                        <div class="info">
+                            <h3>${item.title}</h3>
+                            <p>
+                                ${item.singer}-${item.album}
+                            </p>
+                            <svg class="icon">
+                                <use xlink:href="#icon-play"></use>
+                            </svg>
+                        </div>
+                    </a>
+                </li>`
             }
-            $('.latestMusic>ol').append($li)
+            $node.append($li)
         })
     },
     tabControl(){
@@ -52,14 +59,20 @@ let indexPage = {
         })
         $('.siteNav').on('tabChange',(e,index)=>{
             if(index===1&&$('.tabContent>li').eq(1).attr("isLoaded")!=="yes"){
-                $.get('static/hotList.json').then((response)=>{
-                    console.log(response)
+                $.get('static/hotList.json').then((res)=>{
+                    $('#hotList .loading').remove()
                     $('.tabContent>li').eq(index).attr("isLoaded","yes")
+                    $('.update').text(res.updateTime)
+                    this.render(res.data,$('#hotList .list'))
                 })
+                
             }else if(index===2&&$('.tabContent>li').eq(2).attr("isLoaded")!=="yes"){
-                $.get('static/hotSearch.json').then((response)=>{
-                    console.log(response)
+                $.get('static/hotSearch.json').then((res)=>{
                     $('.tabContent>li').eq(index).attr("isLoaded","yes")
+                    res.hotKeyWord.forEach((kw)=>{
+                        $('.hotWords .words').append(`<li><a href="#">${kw}</a></li>`)
+                    })
+                    
                 })
             }
         })
@@ -67,38 +80,47 @@ let indexPage = {
     hotSearch(){
         let timer = undefined
         $('input.search').on('input',(e)=>{
+            $('#search').addClass('searching')
             let keyword = $(e.currentTarget).val().trim()
+            if($(e.currentTarget).val()==''){return}
             if(timer){
                 clearTimeout(timer)
             }
             timer = setTimeout(()=>{
+                $('.results li').remove()
                 this.search(keyword).then((result)=>{
+                    timer = undefined
                     if(result.length!==0){
-                        $('.results').text(result[0].title)
+                        $('.results .keyword').text(`搜索“${keyword}”`)
+                            this.render(result,$('.results>ol'))
                     }else{
-                        $('.results').text('没有搜索到结果')
+                        $('.results .keyword').text(`搜索“${keyword}”没有结果`)
                     }
                 })
-            },500)
-
+            },1000)
+        })
+        $('.inputbox>.icon-close').on('click',()=>{
+            $('#search').removeClass('searching')
+            $('input').val('')
         })
     },
     search(keyword){
         return new Promise((resolve,reject)=>{
             let dictionary = [
                 { "id": 1, "isSQ": true, "title": "白兰鸽巡游记", "singer": "丢火车乐队", "album": "游歌" },
-                { "id": 2, "isSQ": true, "title": "SoulMate", "singer": "Justin Timberlake", "album": "SoulMate" },
-                { "id": 3, "isSQ": false, "title": "哭砂", "singer": "张惠妹", "album": "A-mei Acoustic Best" },
+                { "id": 2, "isSQ": true, "title": "SoulMate", "singer": "JustinTimberlake", "album": "SoulMate" },
+                { "id": 3, "isSQ": false, "title": "哭砂", "singer": "张惠妹", "album": "A-meiAcousticBest" },
                 { "id": 4, "isSQ": false, "title": "匆匆那年", "singer": "王菲", "album": "匆匆那年" },
                 { "id": 5, "isSQ": true, "title": "Yellow", "singer": "Coldplay", "album": "Yellow" } 
             ]
-            let result = dictionary.filter((item)=>{
-                return item.title.search(keyword)>=0 || item.singer.search(keyword)>=0
-            })
-            setTimeout(()=>{
-                resolve(result)
-            },Math.random()*500+200)
+                let result = dictionary.filter((item)=>{
+                    return item.title.search(keyword)>=0
+                })
+                setTimeout(()=>{
+                    resolve(result)
+                },Math.random()*500+100)
         })
-    }    
+    },
+    
 }
 indexPage.init()
